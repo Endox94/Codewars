a19020e66a9d61156159dc380b682616f748ca40
Oliver Lang <endox.gaming@gmx.de>
2022-08-12 17:08:46 +0200

    Codewars 12.08.22

---
+++ FindParityOutlier.js
@@ -0,0 +1,7 @@
+function findOutlier(integers){
+    if (integers.filter(x => x % 2 == 0).length == 1){
+      return integers.filter(x => x % 2 == 0)[0]
+    } else {
+      return integers.filter(x => x % 2 != 0)[0]
+    }
+  }
