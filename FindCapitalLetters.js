f272d8627d75448a676dbfdde3ecbd426c68a5fc
Oliver Lang <endox.gaming@gmx.de>
2022-08-18 21:56:57 +0200

    Codewars 18.08.22

---
+++ FindCapitalLetters.js
@@ -0,0 +1,11 @@
+function capitals (word) {
+	let arr = [];
+  
+  for (let i = 0; i < word.length; i++){
+    if (word[i] == word[i].toUpperCase()){
+      arr.push(i);
+    }
+  }
+  
+  return arr;
+}
