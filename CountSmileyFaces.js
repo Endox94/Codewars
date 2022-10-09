8c1a1a60e937b2c397b8c81a6cdbdcc105639e65
Oliver Lang <endox.gaming@gmx.de>
2022-10-09 11:30:15 +0200

    Codewars 09.10.22

---
+++ CountSmileyFaces.js
@@ -0,0 +1,13 @@
+function countSmileys(arr) {
+    let count = 0;
+  
+    for (let i = 0; i < arr.length; i++){
+      if ((arr[i][0] == ':' || arr[i][0] == ';') && (arr[i][1] == ')' || arr[i][1] == 'D')){
+        count++;
+      } else if ((arr[i][0] == ':' || arr[i][0] == ';') && (arr[i][1] == '-' || arr[i][1] == '~') && (arr[i][2] == ')' || arr[i][2] == 'D')){
+        count++
+      }
+    }
+    
+    return count;
+  }
